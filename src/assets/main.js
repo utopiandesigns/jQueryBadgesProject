db366d998f1df2a98555a78029cfb5ca6bb55891
Julien Kos <julien@utopiandesigns.co>
2017-02-03 16:00:44 +0100

    Setting up code

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,41 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+      url: 'https://www.codeschool.com/users/julienkos.json',
+      dataType: 'jsonp',
+      success: function(response) {
+          addCourses(response.courses.completed);
+      }
+    });
+
+    function addCourses(courses) {
+
+      var $badges = $('#badges');
+
+      courses.forEach(function(course) {
+
+        console.log(course);
+
+        var $course = $('<div />', {
+          'class': 'course'
+        }).appendTo($badges);
+
+        $('<h3 />', {
+          text: course.title
+        }).appendTo($course);
+
+        $('<img />', {
+          src: course.badge
+        }).appendTo($course);
+
+        $('<a />', {
+          href: course.url,
+          target: "_blank",
+          class: "btn btn-primary",
+          text: "See Course"
+        }).appendTo($course);
+
+      })
+    }
 
 });
